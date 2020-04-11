6e5d51253659e3f2d6768017145cbfeba26182d0
João Pedro Gamin <joaumxp@hotmail.com>
2020-04-11 18:04:32 -0300

    Conceitos de estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-end web']);
 
+  function handleAddProject() {
+    //projects.push(`Novo Projeto ${Date.now()}`);
+    setProjects([...projects,`Novo Projeto ${Date.now()}`]);
+  }
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>menu 1</li>
-        </ul>
-      </Header>
       <Header title="Projects" />
-    </>
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
 
+      <button type="button" onClick={handleAddProject} >Adicionar projeto</button>
+    </>
   );
 }
 
