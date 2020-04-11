e8658fc9dfcd0455a00950af9800901bfb0e573c
João Pedro Gamin <joaumxp@hotmail.com>
2020-04-11 15:24:35 -0300

    conceito de frangments e de componentes aninhados

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
 
-  return <h1>Hello Wolrd</h1>;
+  return (
+    <>
+      <Header />
+      <Header />
+    </>
+
+  );
 }
 
 export default App;
