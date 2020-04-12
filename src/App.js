301e15c1723969907bf5c362ef32d45dfe538cba
João Pedro Gamin <joaumxp@hotmail.com>
2020-04-12 18:39:26 -0300

    Inclusão do axios no projeto e comunicação com api em node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import api from './services/api';
 
 import './App.css';
 import backgroundImg from './assets/background.jpg';
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
 
   function handleAddProject() {
     //projects.push(`Novo Projeto ${Date.now()}`);
-    setProjects([...projects,`Novo Projeto ${Date.now()}`]);
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
   }
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+      console.log(response);
+    });
+  }, []);
+
   return (
     <>
       <Header title="Projects" />
-      <img width={300} src={backgroundImg} alt="Background"/>
+
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject} >Adicionar projeto</button>
+
+      <img width={300} src={backgroundImg} alt="Background" />
     </>
   );
 }
