ca40519f5116889badeab282fe0d9005bcb73c3a
João Pedro Gamin <joaumxp@hotmail.com>
2020-04-12 18:59:15 -0300

    Insclusão de plugin babel pra usar comandos async Possibilidade de criar novos projetos do na api

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ import backgroundImg from './assets/background.jpg';
 function App() {
   const [projects, setProjects] = useState([]);
 
-  function handleAddProject() {
+  async function handleAddProject() {
     //projects.push(`Novo Projeto ${Date.now()}`);
-    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    //setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    const response = await api.post('/projects',
+      {
+        title: `Novo Projeto ${Date.now()}`,
+        owner: "ReactJS"
+      }
+    );
+
+    const newProject = response.data;
+    setProjects([...projects, newProject]);
+
   }
 
   useEffect(() => {
